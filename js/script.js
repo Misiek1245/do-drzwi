2f459b4bb959d9a4fc07f63add03310916adf333
Misiek1245 <embappe7@gmail.com>
2026-04-04 23:58:31 +0200

    adding WORKING (i hope) buttons

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 "use strict";
 console.log("test");
+console.log("x");
+let x = 0;
+const output = document.getElementById("nrm");
 function nrm(event) {
     event.preventDefault();
     const input = document.getElementById("numb");
-    const output = document.getElementById("nrm");
     if (input && output) {
-        const x = input.value;
-        output.innerText = x;
+        x = Number(input.value);
+        output.textContent = x.toString();
+        console.log(x);
     }
+}
+function chgn(n) {
+    x = x + n;
+    output.textContent = x.toString();
 }
--- js/script.ts
+++ js/script.ts
@@ -1,12 +1,17 @@
 console.log("test")
+console.log("x")
+let x: number = 0; 
+const output = document.getElementById("nrm")!
 function nrm(event: Event){
 	event.preventDefault();
 	const input = document.getElementById("numb") as HTMLInputElement | null;
-	const output = document.getElementById("nrm")
-
 	if (input && output){
-		const x = input.value
-		output.innerText = x
+		x = Number(input.value)
+		output.textContent = x.toString()
+		console.log(x)
 	}
 }
-
+function chgn(n :number){
+	x = x + n
+	output.textContent = x.toString()
+}
