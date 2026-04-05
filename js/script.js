6279f511ab956c5f346ad424e688910ea67569ad
Misiek1245 <embappe7@gmail.com>
2026-04-05 16:11:40 +0200

    add sorting mechanic and some buttons

--- js/script.js
+++ js/script.js
@@ -2,7 +2,22 @@
 console.log("test");
 console.log("x");
 let x = 0;
+let multi = 1;
+const numbers = [];
 const output = document.getElementById("nrm");
+function sort() {
+    const imgg = document.querySelector("#sort img");
+    if (!imgg)
+        return;
+    if (multi == 1) {
+        imgg.src = "../img/sort-up.png";
+        multi = -1;
+    }
+    else {
+        imgg.src = "../img/sort-down.png";
+        multi = 1;
+    }
+}
 //numer main
 function nrm(event) {
     event.preventDefault();
@@ -15,6 +30,13 @@ function nrm(event) {
 }
 // change number
 function chgn(n) {
-    x = x + n;
+    x = x + n * multi;
     output.textContent = x.toString();
+    console.log(x);
+}
+function deln() {
+    numbers[x] = 0;
+}
+function c() {
+    location.reload();
 }
--- js/script.ts
+++ js/script.ts
@@ -1,7 +1,20 @@
 console.log("test")
 console.log("x")
 let x: number = 0; 
+let multi: number =1
+const numbers :number[] = []
 const output = document.getElementById("nrm")!
+function sort(){
+	const imgg = document.querySelector("#sort img") as HTMLImageElement | null
+	if (!imgg) return
+	if (multi == 1){
+	imgg.src ="../img/sort-up.png"
+	multi = -1
+	}else {
+	imgg.src ="../img/sort-down.png"
+	multi = 1
+	}
+}
 //numer main
 function nrm(event: Event){
 	event.preventDefault();
@@ -14,6 +27,13 @@ function nrm(event: Event){
 }
 // change number
 function chgn(n :number){
-	x = x + n
+	x = x + n*multi
 	output.textContent = x.toString()
+	console.log(x)
+}
+function deln() {
+	numbers[x] = 0	
+}
+function c() {
+	location.reload()
 }
